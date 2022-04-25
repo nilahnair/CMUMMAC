579e8dd1580b5f4dce4076c2af35e1ed94aed026
Nilah Ravi Nair <nilahnair.27@gmail.com>
2022-04-25 15:31:38 +0200

    Update preprocessing_test.py

--- preprocessing_test.py
+++ preprocessing_test.py
@@ -12,18 +12,18 @@ def process_files():
     for p in range (len(files_path)):
         #print(files_path[p].replace("\SensorData", "\SensorData\Dataset"))
         # Extract subfolders within each folder
-        sub_files_path= [x for x in os.listdir(base_path+"/"+files_path[p])]
+        sub_files_path= [x for x in os.listdir(base_path+files_path[p])]
         print(sub_files_path)
         files_df=[]
         # Iterate through all subfolders within each folder
         for q in range(len(sub_files_path)):
             # Extract files within each subfolder
-            sub_sub_files_path = [x for x in os.listdir(base_path+"/"+files_path[p]+"/"+sub_files_path[q])]
+            sub_sub_files_path = [x for x in os.listdir(base_path+files_path[p]+"/"+sub_files_path[q])]
 
             # Loop through the files and push them into array of dataframes
             for r in range(len(sub_sub_files_path)):
-                print(p,q,r,base_path + "/" + files_path[p] + "/" + sub_files_path[q] + "/" + sub_sub_files_path[r] )
-                compiled_path=base_path + "/" + files_path[p] + "/" + sub_files_path[q] + "/" + sub_sub_files_path[r]
+                print(p,q,r,base_path + files_path[p] + "/" + sub_files_path[q] + "/" + sub_sub_files_path[r] )
+                compiled_path=base_path +files_path[p] + "/" + sub_files_path[q] + "/" + sub_sub_files_path[r]
                 file_data = pd.read_csv(compiled_path,sep='\t',skiprows=1, )
 
                 # Renaming columns
@@ -43,9 +43,9 @@ def process_files():
             
             drop_path='/data/nnair/cmu/'
             merged_df = merged_df[merged_df != 0].dropna() #dropping rows with missing values
-            print(drop_path +'test/'+files_path[p]+'_'+sub_files_path[q]+'.csv')
+            print(drop_path +'test/'+files_path[p]+'/'+sub_files_path[q]+'.csv')
             # Export the DF into a file
-            merged_df.to_csv(drop_path+'test/'+files_path[p]+'_'+sub_files_path[q]+'.csv', index=False, sep=';',)
+            merged_df.to_csv(drop_path+'test/'+files_path[p]+'/'+sub_files_path[q]+'.csv', index=False, sep=';',)
 
 
 process_files()
